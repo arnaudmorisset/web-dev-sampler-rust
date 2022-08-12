1e92336598281bf7a39cb5de980f03cb1ee31cb7
Arnaud Morisset <arnaudmorisset@protonmail.com>
2022-08-12 17:14:09 +0200

    sanitize user input

--- src/routes/signup.rs
+++ src/routes/signup.rs
@@ -1,9 +1,13 @@
 use actix_web::{get, web, HttpResponse, Responder};
+use sanitize_html::rules::predefined::DEFAULT;
+use sanitize_html::sanitize_str;
 
 #[get("/signup/my-name-is/{name}")]
 pub async fn signup(name: web::Path<String>) -> impl Responder {
+    let sanitized_name = sanitize_str(&DEFAULT, name.as_str()).unwrap();
+
     HttpResponse::Ok().body(format!(
         "You're all signed up for the big convention {}!",
-        name.to_string()
+        sanitized_name
     ))
 }
--- tests/signup.rs
+++ tests/signup.rs
@@ -12,3 +12,16 @@ async fn test_signup() {
     let body = test::read_body(res).await;
     assert_eq!(body, "You're all signed up for the big convention Arnaud!");
 }
+
+#[actix_web::test]
+async fn test_signup_escape_html() {
+    let app = test::init_service(App::new().service(signup)).await;
+    let req =
+        test::TestRequest::with_uri("/signup/my-name-is/%3Ci%3EArnaud%3C%2Fi%3E").to_request();
+
+    let res = test::call_service(&app, req).await;
+    assert!(res.status().is_success());
+
+    let body = test::read_body(res).await;
+    assert_eq!(body, "You're all signed up for the big convention Arnaud!");
+}
